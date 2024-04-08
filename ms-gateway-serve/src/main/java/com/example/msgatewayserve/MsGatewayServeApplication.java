e81c9e837887fda8c6e09c04f97ba38773572e53
jherson91fs <jherson.fernandez.19@gmail.com>
2024-04-08 08:54:25 -0500

    fdsfsdf

---
+++ ms-gateway-serve/src/main/java/com/example/msgatewayserve/MsGatewayServeApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgatewayserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatewayServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayServeApplication.class, args);
+	}
+
+}
