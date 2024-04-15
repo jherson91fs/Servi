0fd951df0c616a1e5714036373e2ca0878ee610e
jherson91fs <jherson.fernandez.19@gmail.com>
2024-04-15 11:34:18 -0500

    hgfasd

--- ms-pedido/src/main/java/com/example/mspedido/dto/ProductoDto.java
+++ ms-pedido/src/main/java/com/example/mspedido/dto/ProductoDto.java
@@ -5,13 +5,5 @@ import lombok.Data;
 @Data
 public class ProductoDto {
     private Integer id;
-    private String fechaVenta;
-    private int cantidadProductos;
-    private String metodoPago;
-    private String nombreCliente;
-    private String direccionCliente;
-    private String correoCliente;
-    private String telefonoCliente;
-    private String productoVendido;
-    private double precioProducto;
+    private String nombre;
 }
