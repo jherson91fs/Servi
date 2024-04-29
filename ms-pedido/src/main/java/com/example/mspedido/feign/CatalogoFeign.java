12f0889b4f1e1365b55244e9d33443b2a3d793e5
jherson91fs <jherson.fernandez.19@gmail.com>
2024-04-29 10:36:50 -0500

    resiliencia02

--- ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "catalogo-service",path = "/categoria")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProducto")
     public ResponseEntity<ProductoDto> listById(@PathVariable(required = true) Integer id);
+    default ResponseEntity<ProductoDto> fallbackProducto(Integer id, Exception e){
+        return ResponseEntity.ok(new ProductoDto());
+    }
 }
